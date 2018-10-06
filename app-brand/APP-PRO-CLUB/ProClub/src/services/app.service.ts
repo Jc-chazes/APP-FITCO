7969c93893d0bad64ce73f83790c2229fb022517
Jean-Carlo <jeancarlo@fitco.com.pe>
2018-10-05 19:04:51 -0500

    proclub

--- app-brand/APP-PRO-CLUB/ProClub/src/services/app.service.ts
+++ app-brand/APP-PRO-CLUB/ProClub/src/services/app.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class AppService {
 //  gateway = "https://nodejs.fitcoapp.net";
+//local
  gateway= "https://172.16.17.49:9000";
  arrEstablishmentIds = [4];
 
